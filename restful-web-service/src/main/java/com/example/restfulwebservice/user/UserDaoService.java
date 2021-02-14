c36666029d3c7484f651e87082d907ee7700ebdf
nyju <nyju2104@gmail.com>
2021-02-14 15:17:59 +0900

    restful 사용자 삭제 api 추가

--- restful-web-service/src/main/java/com/example/restfulwebservice/user/UserDaoService.java
+++ restful-web-service/src/main/java/com/example/restfulwebservice/user/UserDaoService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 @Service // @Component 로 사용해도됨
@@ -34,6 +35,19 @@ public class UserDaoService {
     public User findOne(int id) {
         for (User user : users) {
             if (user.getId() == id) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public User deleteById(int id) {
+        Iterator<User> iterator = users.iterator();
+
+        while (iterator.hasNext()) { // 순차적 조회
+            User user = iterator.next();
+            if (user.getId() == id) {
+                iterator.remove();
                 return user;
             }
         }
