c0ef156a46a9ee9678ef3d7942c2e289368f2c8e
nyju <nyju2104@gmail.com>
2021-03-05 00:04:56 +0900

    o API버전관리 1. URI를 통한 버전관리 2. Request 파라미터를 이용한 버전관리 3. MIME-Type 을 이용한 버전관리 4. 헤더값을 이용한 버전관리

--- restful-web-service/src/main/java/com/example/restfulwebservice/user/AdminUserController.java
+++ restful-web-service/src/main/java/com/example/restfulwebservice/user/AdminUserController.java
@@ -39,7 +39,10 @@ public class AdminUserController {
     }
 
     // GET /admin/users/1 -> /admin/v1/users/1
-    @GetMapping("/v1/users/{id}")
+    // @GetMapping("/v1/users/{id}") // URI를 이용한 버전관리
+    // @GetMapping(value = "/users/{id}/", params = "version=1") // request 파라미터를 이용한 버전관리
+    // @GetMapping(value="/users/{id}", headers="X-API-VERSION=1") // 헤더값을 이용한 버전관리
+    @GetMapping(value = "/users/{id}", produces = "application/vnd.company.appv1+json") // 마임타임을 이용한 방법
     public MappingJacksonValue retrieveUserV1(@PathVariable int id) {
         User user = service.findOne(id);
 
@@ -58,7 +61,10 @@ public class AdminUserController {
     }
 
 
-    @GetMapping("/v2/users/{id}")
+    // @GetMapping("/v2/users/{id}")
+    // @GetMapping(value = "/users/{id}/", params = "version=2")
+    // @GetMapping(value="/users/{id}", headers="X-API-VERSION=2")
+    @GetMapping(value = "/users/{id}", produces = "application/vnd.company.appv2+json")
     public MappingJacksonValue retrieveUserV2(@PathVariable int id) {
         User user = service.findOne(id);
 
