a6e12cd64d9a11dc1de915576fab321c16bdcfcc
nyju <nyju2104@gmail.com>
2021-03-21 16:45:31 +0900

    Swagger Documentation 커스텀

--- restful-web-service/src/main/java/com/example/restfulwebservice/config/SwaggerConfig.java
+++ restful-web-service/src/main/java/com/example/restfulwebservice/config/SwaggerConfig.java
@@ -1,24 +1,37 @@
 package com.example.restfulwebservice.config;
 
 
-import io.swagger.models.Contact;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.hateoas.client.LinkDiscoverer;
 import org.springframework.hateoas.client.LinkDiscoverers;
 import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
 import org.springframework.plugin.core.SimplePluginRegistry;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
+    //연락처 정보를 위한 객체 생성
+    private static final Contact DEFAULT_CONTACT = new Contact("NyJu",
+            "https://github.com/nyju", "nyju2104@gmail.com");
+
+    //API 정보를 위한 객체 생성
+    private static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Awesome API Title",
+            "My User management REST API service", "1.0", "urn:tos", DEFAULT_CONTACT, "Apache 2.0",
+            "http://apache.org/licenses/LICENSE-2.0", new ArrayList<>());
+
+    // 어떤 형태로 데이터를 사용할 수 있는지 데이터 타입 설정
+    private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<>(Arrays.asList("application/json", "application/xml"));
 
     @Bean
     public LinkDiscoverers discoverers() {
@@ -30,7 +43,10 @@ public class SwaggerConfig {
 
     @Bean
     public Docket api(){
-        return new Docket(DocumentationType.SWAGGER_2);
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(DEFAULT_API_INFO)
+                .produces(DEFAULT_PRODUCES_AND_CONSUMES)
+                .consumes(DEFAULT_PRODUCES_AND_CONSUMES);
     }
 }
 
